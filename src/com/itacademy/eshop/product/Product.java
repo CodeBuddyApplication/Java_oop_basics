ef34ce53cd1fdbcf15fadc1e44969541cad108c2
ZilvinasVysniauskas <zilvinas.vysniauskas99@gmail.com>
2023-03-03 22:14:19 +0200

    task ready

---
+++ src/com/itacademy/eshop/product/Product.java
@@ -0,0 +1,54 @@
+package com.itacademy.eshop.product;
+
+import com.itacademy.eshop.product.types.Category;
+
+import java.util.ArrayList;
+
+public class Product {
+    private String name;
+    private double price;
+    private Category category;
+    private ArrayList<Review> reviews;
+
+    public Product(String name, double price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.reviews = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public ArrayList<Review> getReviews() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        reviews.add(review);
+    }
+
+    public Integer getAverageRating() {
+        if (reviews.isEmpty()) {
+            return null;
+        }
+        int rating = 0;
+        for (Review review : reviews) {
+            rating += review.getRating();
+        }
+        return rating / reviews.size();
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/com/itacademy/eshop/shop/ShoppingCart.java
@@ -0,0 +1,29 @@
+package com.itacademy.eshop.shop;
+
+import com.itacademy.eshop.product.Product;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    ArrayList<Product> products;
+
+    public ShoppingCart() {
+        this.products = new ArrayList<>();
+    }
+
+    public void printShoppingCart() {
+        for (Product product : products) {
+            System.out.println("Product name: " + product.getName());
+            System.out.println("Price: " + product.getPrice());
+            System.out.println("Category: " + product.getCategory());
+        }
+        System.out.println("Total price: " + getTotalPrice());
+    }
+
+    public String getTotalPrice() {
+        /**
+         * Cakculates total price of shopping cart
+         */
+        throw new UnsupportedOperationException("getTotalPrice() is not implemented yet");
+    }
+}
